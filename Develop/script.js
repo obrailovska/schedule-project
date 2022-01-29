6eaab23a385069f8fba628b892b8952bff88f056
Olha Brailovska <obrailovska@Olhas-Air.home>
2022-01-28 23:02:52 -0500

    added more time slots

--- Develop/script.js
+++ Develop/script.js
@@ -10,16 +10,16 @@ $(document).ready(function () {
     localStorage.setItem(time, note);
   });
 
-  function colotTimeSlot() {
-    var cHour = moment().hours();
-    console.log(cHour);
+  function colorTimeSlot() {
+    var currentHour = moment().hours();
+    console.log(currentHour);
 
     $(".time-block").each(function () {
-      var bHour = parseInt($(this).attr("id").split("-")[1]);
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-      if (bHour < cHour) {
+      if (blockHour < currentHour) {
         $(this).addClass("past");
-      } else if (bHour === cHour) {
+      } else if (blockHour === currentHour) {
         $(this).removeClass("past");
         $(this).addClass("present");
       } else {
@@ -28,7 +28,15 @@ $(document).ready(function () {
       }
     });
   }
-  colotTimeSlot();
+  colorTimeSlot();
 
   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 });
